b277fdcbd107473754d84d05cebc8f9b9fabf9f6
Your Name <you@example.com>
2017-12-08 06:04:57 +0000

    Extract argument parsing to separate function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,51 @@
 #include "calc.compute.h"
 #include "calc.ast.h"
 #include "calc.h"
+
+typedef struct config_t {
+  arg_x_t arg_x;
+  char * expr;
+} config_t;
+
+bool parse_args (config_t * config, int argc, char * argv[])
+{
+  config->arg_x.has_x = false;
+
+  if (getopt (argc, argv, "x:") == 'x')
+    {
+      char * end;
+      config->arg_x.has_x = true;
+      config->arg_x.x = strtold (optarg, &end);
+      if (*end != '\0')
+        {
+          fprintf (stderr, "x must be a number (parse fail at %td in '%s')\n", end - optarg, optarg);
+          return (false);
+        }
+    }
+
+  int expr_size = 1;
+  int i;
+  for (i = optind; i < argc; i++)
+    expr_size += strlen (argv[i]) + 1;
+
+  config->expr = malloc (expr_size);
+  if (NULL == config->expr)
+    return (false);
+
+  char * ptr = config->expr;
+  char * arg;
+  for (i = optind; i < argc; ++i)
+    {
+      for (arg = argv[i]; *arg; )
+        *ptr++ = *arg++;
+      *ptr++ = ' ';
+    }
+  if (optind != argc)
+    --ptr;
+  *ptr = 0;
+
+  return (true);
+}
 
 int run_calc (parser_funcs_t parser_funcs, char * expr, arg_x_t arg_x)
 {
@@ -35,47 +81,22 @@ int run_calc (parser_funcs_t parser_funcs, char * expr, arg_x_t arg_x)
 
 int main (int argc, char * argv[])
 {
-  arg_x_t arg_x = { .has_x = false };
+  config_t config;
+  if (!parse_args (&config, argc, argv))
+    return (EXIT_FAILURE);
 
-  if (getopt (argc, argv, "x:") == 'x')
-    {
-      char * end;
-      arg_x.has_x = true;
-      arg_x.x = strtold (optarg, &end);
-      if (*end != '\0')
-        {
-          fprintf (stderr, "x must be a number (parse fail at %td in '%s')\n", end - optarg, optarg);
-          return (EXIT_FAILURE);
-        }
-    }
+  int rv_compute = run_calc (compute_parser, config.expr, config.arg_x);
+  int rv_ast_rec = run_calc (ast_parser_rec, config.expr, config.arg_x);
+  int rv_ast_iter = run_calc (ast_parser_iter, config.expr, config.arg_x);
 
-  int buf_size = 1;
-  int i;
-  for (i = optind; i < argc; i++)
-    buf_size += strlen (argv[i]) + 1;
-
-  char buf[buf_size];
-  char * ptr = buf;
-  char * arg;
-  for (i = optind; i < argc; ++i)
-    {
-      for (arg = argv[i]; *arg; )
-        *ptr++ = *arg++;
-      *ptr++ = ' ';
-    }
-  if (optind != argc)
-    --ptr;
-  *ptr = 0;
-
-  int rv_compute = run_calc (compute_parser, buf, arg_x);
-  int rv_ast_rec = run_calc (ast_parser_rec, buf, arg_x);
-  int rv_ast_iter = run_calc (ast_parser_iter, buf, arg_x);
+  int rv = EXIT_SUCCESS;
   if (0 != rv_compute)
-    return (rv_compute);
-  if (0 != rv_ast_rec)
-    return (rv_ast_rec);
-  if (0 != rv_ast_iter)
-    return (rv_ast_iter);
+    rv = rv_compute;
+  else if (0 != rv_ast_rec)
+    rv = rv_ast_rec;
+  else if (0 != rv_ast_iter)
+    rv = rv_ast_iter;
 
-  return (EXIT_SUCCESS);
+  free (config.expr);
+  return (rv);
 }
